ba0e9bec17c1547771cc2d23d6abb0c069c33212
DipakGj <129220037+DipakGj@users.noreply.github.com>
2025-05-31 14:15:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "IK~SEwCgKIR#8mZj_IvhCTxiEQaAMmAUzQPEy0_Zs_zFfRlUq82F1WQ",
+SESSION_ID: process.env.SESSION_ID || "IK~nNIBGaQR#T8CfmHXH3Pd4zQ3RALzYGIdgIYqBTtacsActVZkmIec",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
