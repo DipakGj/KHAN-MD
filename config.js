64866c69e911fdb2ca586272f9348f90f80d097d
DipakGj <129220037+DipakGj@users.noreply.github.com>
2025-06-10 10:07:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "IK~xfFSiCjR#rvsHSh4XymUY3m_XjC2R6vXE1zw3dZn7T7amkpCdIog",
+SESSION_ID: process.env.SESSION_ID || "IK~VClUUZgY#4wAO6iNctr1XgxpZiWszEG24jwXUcvM0CUiAfa_81TI",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -15,7 +15,7 @@ AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
 AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "",
 // set the auto reply massage on status reply  
-WELCOME: process.env.WELCOME || "false",
+WELCOME: process.env.WELCOME || "true",
 // true if want welcome and goodbye msg in groups    
 ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
 // make true to know who dismiss or promoted a member in group
@@ -31,7 +31,7 @@ BOT_NAME: process.env.BOT_NAME || "BABAI",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "Alone-Babai",
 // type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
 // make this true for custum emoji react    
 CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,💔,🤍,😂,🙏⚡,🫦,🖕,",
 // chose custom react emojis by yourself 
